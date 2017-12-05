a0042f8cb884151757926445db6e6d6339c2cd03
Pay <Pay@DESKTOP-QU8ICUT>
2017-12-05 15:00:23 +0800

    搭建核心框架

---
+++ MusicWeb/src/main/java/com/music/test/musicAPIAction.java
@@ -0,0 +1,9 @@
+package com.music.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/musicAPIAction")
+public class musicAPIAction {
+}
